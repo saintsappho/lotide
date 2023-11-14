137d03e1b6d8bb2ba1b7866ce3c55ffa6c27da86
Robin Fleur <robinrosefleur@gmail.com>
2023-11-14 04:05:44 +0000

    tail.js completion

--- tail.js
+++ tail.js
@@ -14,11 +14,10 @@ const tail = function(array) {
   return array.slice(1);
 };
 let result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(result[1], "Labs");
+assertEqual(result.length, 2); 
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs"); 
 let result2 = tail([]);
-assertEqual(result2.length, 0); // ensure we get back two elements
-assertEqual(result2[0], undefined) // ensure first element is "Lighthouse"
+assertEqual(result2.length, 0); 
+assertEqual(result2[0], undefined) 
 assertEqual(result2[1], undefined);
