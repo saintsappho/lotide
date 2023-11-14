cac693db325ce14fdfeb7af214e07412187dc705
Robin Fleur <robinrosefleur@gmail.com>
2023-11-14 01:48:27 +0000

    changes made, struggling with git

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍🏽 Assertion Passed 👍🏽");
+    console.log("👍🏽 Assertion Passed: " + actual + " === " + expected + ". 👍🏽");
   } else {
     console.log(" 💣🩸🔪 Assertion Failed: " + actual + " !== " + expected + ". 🔪🩸💣 ");
   }
 };
 assertEqual("Butts", "butts");
 assertEqual('butts', 'butts');
+assertEqual(5, -5);
+assertEqual(5, 5);
