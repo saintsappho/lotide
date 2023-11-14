ac5757c3b1639ea77387dbc2be7c13b508f1bd1d
Robin Fleur <robinrosefleur@gmail.com>
2023-11-14 02:05:48 +0000

    template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍🏽 Assertion Passed: " + actual + " === " + expected + ". 👍🏽");
+    console.log(`👍🏽 Assertion Passed: ${actual} === ${expected}. 👍🏽`);
   } else {
-    console.log(" 💣🩸🔪 Assertion Failed: " + actual + " !== " + expected + ". 🔪🩸💣 ");
+    console.log(` 💣🩸🔪 Assertion Failed: ${actual} !== ${expected}. 🔪🩸💣 `);
   }
 };
 assertEqual("Butts", "butts");
