2824065c7ee5b415689e6a242fb4fdf435831709
saintsappho <robinrosefleur@gmail.com>
2023-11-14 22:14:57 -0700

    without.js

--- without.js
+++ without.js
@@ -1,5 +1,5 @@
 const without = function(array1, array2) {
-    var newArray = []
+    let newArray = []
     for (let i = 0; i < array1.length; i++) {
         if (!array2.includes(array1[i])) {
             newArray.push(array1[i]);
