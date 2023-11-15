ced7aa5704108228243bedef0662784b4e9cabf4
saintsappho <robinrosefleur@gmail.com>
2023-11-14 22:13:46 -0700

    without.js

---
+++ without.js
@@ -0,0 +1,21 @@
+const without = function(array1, array2) {
+    var newArray = []
+    for (let i = 0; i < array1.length; i++) {
+        if (!array2.includes(array1[i])) {
+            newArray.push(array1[i]);
+        }
+    } return newArray;
+};
+console.log(without([1, 2, 3], [1, 2]));
+console.log(without([1, 2, 3], [2]));
+console.log(without([1, 2], '2'));
+console.log(without([1, 2, 3], [1, 2, 3,]));
+console.log(without([1, 2, 3], [3, 2, 1]));
+console.log(without([1, 2, 3], [4]));
+console.log(without([1, 2, 3], [123]));
+console.log(without([1], [1]));
+console.log(without([0], []));
+console.log(without([1], [0]));
+console.log(without([1, 2, 3], []));
+console.log(without(["1", "2", "3"], ["1", "2", "3"]));
+console.log(without(["1", "2", "3"], ['1', '2', 3]));
