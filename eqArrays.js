3c487cf41e285206e4819b90b5561301f36bf341
saintsappho <robinrosefleur@gmail.com>
2023-11-14 21:41:09 -0700

    eqArrays Complete

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
   const assertEqual = function(actual, expected) {
     if (actual === expected) {
-      console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+      console.log(` 🟢🟢🟢 Assertion Passed: ${actual} === ${expected} 🟢🟢🟢 `);
     }
   
     if (actual !== expected) {
-      console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+      console.log(` 🟥🟥🟥 Assertion Failed: ${actual} !== ${expected} 🟥🟥🟥 `);
     }
   };
 const eqArrays = function(array1, array2) {
@@ -18,14 +18,14 @@ const eqArrays = function(array1, array2) {
 };
 assertEqual(eqArrays([1], [2]), false);
 assertEqual(eqArrays([1, 2], [1, 2]), true);
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3,])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays([1], [1])); // => true
-console.log(eqArrays([1], [2])); // => false
-console.log(eqArrays([1], [0])); // => false
-console.log(eqArrays([], [])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ['1', '2', 3])); // => false
+assertEqual(eqArrays([1, 2], '1, 2'), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3,]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1], [1]), true); // => true
+assertEqual(eqArrays([1], [2]), false); // => false
+assertEqual(eqArrays([1], [0]), false); // => false
+assertEqual(eqArrays([], []), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ['1', '2', 3]), false); // => false
