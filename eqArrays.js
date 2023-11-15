0da04e046d91cbfb4e82e7305369d2afdd71fabd
saintsappho <robinrosefleur@gmail.com>
2023-11-14 20:52:02 -0700

    asking AI for help

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,32 @@
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
-      console.log(`👍🏽 Assertion Passed: ${actual} === ${expected}. 👍🏽`);
+      return 'True';
     } else {
-      console.log(` 💣🩸🔪 Assertion Failed: ${actual} !== ${expected}. 🔪🩸💣 `);
+      return 'False';
     }
   };
   
   const eqArrays = function(array1, array2){
-    for (i = 0; i < array1.length; i++) {
-        assertEqual(array1[i], array2[i])
+    if (array1.length !== array2.length){
+        return 'False'
+    }
+    for (let i = 0; i < array1.length; i++) {
+        if (assertEqual(array1[i], array2[i])) {
+            continue
+        } else {
+            return 'False'
+        }
     }
   }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [1, 2, 3, ]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1, 2, 3], [1, 2, 3, 4]) // => false
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1], [1]) // => true
+eqArrays([1], [2]) // => false
+eqArrays([1], [0]) // => false
+eqArrays([], []) // => true
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ['1', '2', 3]) // => false
