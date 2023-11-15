8c80593439d29d1c8b95632258783d1a88fef4e0
saintsappho <robinrosefleur@gmail.com>
2023-11-14 21:55:32 -0700

    eqArrays Complete

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
 const assertArraysEqual = function(arrayAssert1, arrayAssert2) {
     if (eqArrays(arrayAssert1, arrayAssert2)) {
-      console.log(` 🟢🟢🟢 Assertion Passed: ${arrayAssert1} === ${arrayAssert2} 🟢🟢🟢 `);
+        console.log(` 🟢🟢🟢 Assertion Passed: ${arrayAssert1} === ${arrayAssert2} 🟢🟢🟢 `);
     }
-  
+
     if (!eqArrays(arrayAssert1, arrayAssert2)) {
-      console.log(` 🟥🟥🟥 Assertion Failed: ${arrayAssert1} !== ${arrayAssert2} 🟥🟥🟥 `);
+        console.log(` 🟥🟥🟥 Assertion Failed: ${arrayAssert1} !== ${arrayAssert2} 🟥🟥🟥 `);
     }
-  };
+};
 const eqArrays = function(array1, array2) {
     if (array1.length !== array2.length) {
         return false;
@@ -16,16 +16,16 @@ const eqArrays = function(array1, array2) {
         }
     } return true;
 };
-assertArraysEqual(eqArrays([1], [2]), false);
-assertArraysEqual(eqArrays([1, 2], [1, 2]), true);
-assertArraysEqual(eqArrays([1, 2], '1, 2'), false);
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3,]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => false
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertArraysEqual(eqArrays([1], [1]), true); // => true
-assertArraysEqual(eqArrays([1], [2]), false); // => false
-assertArraysEqual(eqArrays([1], [0]), false); // => false
-assertArraysEqual(eqArrays([], []), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ['1', '2', 3]), false); // => false
+assertArraysEqual([1], [2]);
+assertArraysEqual([1, 2], [1, 2]);
+assertArraysEqual([1, 2], '1, 2');
+assertArraysEqual([1, 2, 3], [1, 2, 3,]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1], [1]);
+assertArraysEqual([1], [2]);
+assertArraysEqual([1], [0]);
+assertArraysEqual([], []);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ['1', '2', 3]);
