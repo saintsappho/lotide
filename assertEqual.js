2a5f4459213390e41f27d117e27fd23e06b1c6b7
saintsappho <robinrosefleur@gmail.com>
2023-11-14 21:10:21 -0700

    eqArrays Complete

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍🏽 Assertion Passed: ${actual} === ${expected}. 👍🏽`);
+    return true;
   } else {
-    console.log(` 💣🩸🔪 Assertion Failed: ${actual} !== ${expected}. 🔪🩸💣 `);
+    return false;
   }
 };
 assertEqual("Butts", "butts");
--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,23 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return 'True';
-    } else {
-      return 'False';
-    }
-  };
-  
-  const eqArrays = function(array1, array2){
-    if (array1.length !== array2.length){
-        return 'False'
+const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
     }
     for (let i = 0; i < array1.length; i++) {
-        if (assertEqual(array1[i], array2[i])) {
-            continue
+        if (array1[i] === array2[i]) {
         } else {
-            return 'False'
+            return false;
         }
     }
-  }
+    return true;
+};
 
-eqArrays([1, 2, 3], [1, 2, 3, ]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays([1, 2, 3], [1, 2, 3, 4]) // => false
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays([1], [1]) // => true
-eqArrays([1], [2]) // => false
-eqArrays([1], [0]) // => false
-eqArrays([], []) // => true
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ['1', '2', 3]) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3,])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays([1], [1])); // => true
+console.log(eqArrays([1], [2])); // => false
+console.log(eqArrays([1], [0])); // => false
+console.log(eqArrays([], [])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ['1', '2', 3])); // => false
