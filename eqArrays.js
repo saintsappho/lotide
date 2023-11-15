ee7bc97d158069d1ad865f72b2742b10b74b0d1a
saintsappho <robinrosefleur@gmail.com>
2023-11-14 21:14:14 -0700

    eqArrays Complete

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,11 @@
 const eqArrays = function(array1, array2) {
     if (array1.length !== array2.length) {
         return false;
-    }
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] === array2[i]) {
-        } else {
+    } for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
             return false;
         }
-    }
-    return true;
+    } return true;
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3,])); // => true
