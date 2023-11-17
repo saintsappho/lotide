b7bd85c15e845e1c7f83ce640eed12c2eb195b7c
saintsappho <robinrosefleur@gmail.com>
2023-11-16 21:15:15 -0700

    added inspect integration

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -3,7 +3,7 @@ const assertObjectsEqual = function(actual, expected) {
   if (!eqObjects(actual, expected)) {
     console.log(`🟥🟥🟥 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)} 🟥🟥🟥 `);
   } if (eqObjects(actual, expected)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} !== ${inspect(expected)} 🟢🟢🟢`);
+    console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)} 🟢🟢🟢`);
   }
   return;
 };
@@ -14,6 +14,19 @@ const eqObjects = function(object1, object2) {
     if (object1[key] !== object2[key]) {
       return false;
     }
+    if (object1[key].isArray) {
+      eqArrays(object1[key], object2[key])
+    }
+  } return true;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+      return false;
+  } for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+          return false;
+      }
   } return true;
 };
 
