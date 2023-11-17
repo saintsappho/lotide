255e435e88c5965797bc3339ddb273e60fcd7445
saintsappho <robinrosefleur@gmail.com>
2023-11-16 21:06:30 -0700

    AI aid 2

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,26 @@
 const assertObjectsEqual = function(actual, expected) {
   if (!eqObjects(actual, expected)) {
     console.log(`🟥🟥🟥 Assertion Failed 🟥🟥🟥 `);
-  } 
-console.log(`🟢🟢🟢 Assertion Passed 🟢🟢🟢`)
-return;
+  } if (eqObjects(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed 🟢🟢🟢`);
+  }
+  return;
 };
-  
 
 const eqObjects = function(object1, object2) {
-  if (object1.length !== object2.length) return false 
-  for(let key in object1){
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+  for (let key in object1) {
     if (object1[key] !== object2[key]) {
       return false;
-      
-    } 
+    }
   } return true;
-  };
+};
 
 // ----------------------------- TEST -----------------------------
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-assertObjectsEqual(shirtObject , anotherShirtObject)
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject);
 
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(shirtObject , longSleeveShirtObject)
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject, longSleeveShirtObject);
