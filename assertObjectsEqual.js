54bc56df6fe204fc77cf3619610fcf8ebc0dad39
saintsappho <robinrosefleur@gmail.com>
2023-11-16 20:59:12 -0700

    AI aid

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,30 +1,29 @@
 const assertObjectsEqual = function(actual, expected) {
     //loop here to go through key value pair andn check with actual and expected  
     for(let key in actual){
-      if (actual.key !== expected.key) {
+      if (!eqObjects(actual, expected)) {
         console.log(`🟥🟥🟥 Assertion Failed 🟥🟥🟥 `);
-        
       } 
     } console.log(`🟢🟢🟢 Assertion Passed 🟢🟢🟢`)
-      return;
+    return;
   };
   
 
 const eqObjects = function(object1, object2) {
-    //loop here to go through key value pair andn check with object1 and object2  
-    for(let key in object1){
-      if (object1[key] !== object2[key]) {
-        return false;
-        
-      } 
-    } return true;
+  if (object1.length !== object2.length) return false 
+  for(let key in object1){
+    if (object1[key] !== object2[key]) {
+      return false;
+      
+    } 
+  } return true;
   };
 
 // ----------------------------- TEST -----------------------------
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
-assertObjectsEqual(eqObjects(shirtObject , anotherShirtObject), true)
+assertObjectsEqual(shirtObject , anotherShirtObject)
 
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+assertObjectsEqual(shirtObject , longSleeveShirtObject)
