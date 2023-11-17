f1c4952036baa1d51027f73f4b5ae4cde79bd801
saintsappho <robinrosefleur@gmail.com>
2023-11-16 18:15:06 -0700

    Map.js completion

--- map.js
+++ map.js
@@ -26,6 +26,6 @@ const map = function(array, callback){
    } return results; 
 }
 
-console.log(assertArraysEqual(results1 = map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]));
-console.log(assertArraysEqual(results1 = map(words, word => word.length), [ 6, 7, 2, 5, 3]));
-console.log(assertArraysEqual(results1 = map(words, word => word.includes('t')), [ false, true, true, false, true ]));
+console.log(assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]));
+console.log(assertArraysEqual(map(words, word => word.length), [ 6, 7, 2, 5, 3]));
+console.log(assertArraysEqual(map(words, word => word.includes('t')), [ false, true, true, false, true ]));
