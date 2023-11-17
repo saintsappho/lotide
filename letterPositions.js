392217ddcbae971a9b624374df58590850a77c6e
saintsappho <robinrosefleur@gmail.com>
2023-11-16 20:41:36 -0700

    a few touch ups

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,12 @@
-const assertObjectEqual = function(actual, expected) { 
-    for(let key in actual){
-      if (!eqArrays(actual[key], expected[key])) {
-        return (`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
-        
-      } 
-    } return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  };
-  
+const assertObjectEqual = function(actual, expected) {
+    for (let key in actual) {
+        if (!eqArrays(actual[key], expected[key])) {
+            return (`🟥🟥🟥 Assertion Failed 🟥🟥🟥`);
+
+        }
+    } return (`🟢🟢🟢 Assertion Passed 🟢🟢🟢`);
+};
+
 const eqArrays = function(array1, array2) {
     if (array1.length !== array2.length) {
         return false;
@@ -20,29 +20,28 @@ const eqArrays = function(array1, array2) {
 // --------------------
 const letterPositions = function(sentence) {
     const results = {};
-    let i = 0
-    for (letter of sentence){
+    let i = 0;
+    for (letter of sentence) {
         if (letter !== ' ')
             if (results[letter]) {
                 results[letter].push(i);
             } else {
                 results[letter] = [i];
-            } i++
+            } i++;
     }
-    console.log(results)
     return results;
-  };
+};
 
 
-console.log(assertObjectEqual(letterPositions("lighthouse in the house"), { 
-    l: [ 0 ],
-    i: [ 1, 11 ],
-    g: [ 2 ],
-    h: [ 3, 5, 15, 18 ],
-    t: [ 4, 14 ],
-    o: [ 6, 19 ],
-    u: [ 7, 20 ],
-    s: [ 8, 21 ],
-    e: [ 9, 16, 22 ],
-    n: [ 12 ]
-  }), )
+console.log(assertObjectEqual(letterPositions("lighthouse in the house"), {
+    l: [0],
+    i: [1, 11],
+    g: [2],
+    h: [3, 5, 15, 18],
+    t: [4, 14],
+    o: [6, 19],
+    u: [7, 20],
+    s: [8, 21],
+    e: [9, 16, 22],
+    n: [12]
+}),);
