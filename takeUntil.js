0564d9e29a00c275ead3c813f12c477a05477ab5
saintsappho <robinrosefleur@gmail.com>
2023-11-16 18:40:13 -0700

    takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -27,10 +27,10 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(assertArraysEqual((results1), [ 1, 2, 5, 7, 2 ]));
+console.log(assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]));
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(assertArraysEqual((results2), [ "I've", 'been', 'to', 'Hollywood' ]));
+console.log(assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]));
