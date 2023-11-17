c02c841335dfaf6cf6060d2c7844da4a9a8900e9
saintsappho <robinrosefleur@gmail.com>
2023-11-16 21:11:18 -0700

    added inspect integration

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,8 +1,9 @@
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (!eqObjects(actual, expected)) {
-    console.log(`🟥🟥🟥 Assertion Failed 🟥🟥🟥 `);
+    console.log(`🟥🟥🟥 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)} 🟥🟥🟥 `);
   } if (eqObjects(actual, expected)) {
-    console.log(`🟢🟢🟢 Assertion Passed 🟢🟢🟢`);
+    console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} !== ${inspect(expected)} 🟢🟢🟢`);
   }
   return;
 };
